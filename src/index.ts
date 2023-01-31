189e9b9ee069dc7bb04b7f57a3cc021329d3af90
Nate Agrin <nate@exploreomni.com>
2023-01-30 16:10:26 -0800

    checkpoint some wonky attempts at learning about cm6's parser implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { parser } from "./syntax.grammar";
+// import { parser } from "./syntax.grammar";
 import {
   LRLanguage,
   LanguageSupport,
@@ -6,32 +6,66 @@ import {
   foldNodeProp,
   foldInside,
   delimitedIndent,
+  Language,
+  defineLanguageFacet,
+  StreamLanguage,
 } from "@codemirror/language";
 import { styleTags, tags as t } from "@lezer/highlight";
+import { Input, Parser, PartialParse, TreeFragment } from "@lezer/common";
+import { yaml as legacyYaml } from "@codemirror/legacy-modes/mode/yaml";
 
-export const YamlLanguage = LRLanguage.define({
-  parser: parser.configure({
-    props: [
-      indentNodeProp.add({
-        Application: delimitedIndent({ closing: ")", align: false }),
-      }),
-      foldNodeProp.add({
-        Application: foldInside,
-      }),
-      styleTags({
-        Identifier: t.variableName,
-        Boolean: t.bool,
-        String: t.string,
-        LineComment: t.lineComment,
-        "( )": t.paren,
-      }),
-    ],
-  }),
-  languageData: {
-    commentTokens: { line: ";" },
-  },
-});
+class Nonsense implements PartialParse {
+  advance() {
+    return null;
+  }
+  get parsedPos() {
+    return 0;
+  }
+  stopAt(pos: number): void {}
+  get stoppedAt() {
+    return 0;
+  }
+}
 
+class DemoLang extends Parser {
+  createParse(
+    input: Input,
+    fragments: readonly TreeFragment[],
+    ranges: readonly { from: number; to: number }[]
+  ): PartialParse {
+    console.log(input, fragments, ranges);
+    return new Nonsense();
+  }
+}
+
+// export const YamlLanguage = LRLanguage.define({
+//   parser: parser.configure({
+//     props: [
+//       indentNodeProp.add({
+//         Application: delimitedIndent({ closing: ")", align: false }),
+//       }),
+//       foldNodeProp.add({
+//         Application: foldInside,
+//       }),
+//       styleTags({
+//         Identifier: t.variableName,
+//         Boolean: t.bool,
+//         String: t.string,
+//         LineComment: t.lineComment,
+//         "( )": t.paren,
+//       }),
+//     ],
+//   }),
+//   languageData: {
+//     commentTokens: { line: ";" },
+//   },
+// });
+
+// const parser = new DemoLang();
+// const data = defineLanguageFacet({ block: { open: "<!--", close: "-->" } });
+// export const YamlLanguage = new Language(data, parser, [], "yaml");
+
+export const YamlLanguage = StreamLanguage.define(legacyYaml);
 export function Yaml() {
   return new LanguageSupport(YamlLanguage);
 }
