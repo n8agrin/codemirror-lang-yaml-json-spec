ffd7099a92a3a548b83affa5c33a87535568681f
Nate Agrin <nate@exploreomni.com>
2023-01-31 10:09:40 -0800

    Add yaml doc for example

--- examples/basic/index.js
+++
@@ -1,7 +0,0 @@
-import { EditorView, basicSetup } from "codemirror";
-import { Yaml } from "codemirror-lang-yaml";
-
-new EditorView({
-  extensions: [basicSetup, Yaml()],
-  parent: document.body,
-});
