a5ee09ab47cb0e076d8eb870db3315f025e22ec6
Nate Agrin <nate@exploreomni.com>
2023-01-31 09:58:04 -0800

    Update package description and name

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import { LanguageSupport, StreamLanguage } from "@codemirror/language";
 import { yaml as legacyYaml } from "@codemirror/legacy-modes/mode/yaml";
 
 export const YamlLanguage = StreamLanguage.define(legacyYaml);
-
 export const Yaml = () => new LanguageSupport(YamlLanguage);
