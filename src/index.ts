58fd1de499dcc8483168e52d294b20b87c92325a
Nate Agrin <nate@exploreomni.com>
2023-02-01 11:21:17 -0800

    push to test remix

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { yaml as legacyYaml } from "@codemirror/legacy-modes/mode/yaml";
 
 export const YamlLanguage = StreamLanguage.define(legacyYaml);
 export const Yaml = () => new LanguageSupport(YamlLanguage);
-export { YamlJSONSchemaService } from "./YamlJSONSchemaService";
+// export { YamlJSONSchemaService } from "./YamlJSONSchemaService";
